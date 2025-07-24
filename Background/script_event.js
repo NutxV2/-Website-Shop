9680c9cd7089a821938fba7c57776221f9f17e22
NutxV2 <nganyanwainakarin@gmail.com>
2025-07-25 01:29:34 +0700

    Delete Background directory

--- Background/script_event.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('scroll', function () {
-    const container = document.querySelector('.container');
-    if (window.scrollY > 40) {
-        container.classList.add('shrink');
-    } else {
-        container.classList.remove('shrink');
-    }
-});
--- Background/script_event_scrollArrow.js
+++
@@ -1,7 +0,0 @@
-  window.addEventListener('scroll', function () {
-    const arrow = document.querySelector('.scroll-arrow');
-    const scrollBottom = window.innerHeight + window.scrollY >= document.body.offsetHeight - 1;
-
-    arrow.style.opacity = scrollBottom ? '0' : '0.7';
-    arrow.style.pointerEvents = scrollBottom ? 'none' : 'auto';
-  });
